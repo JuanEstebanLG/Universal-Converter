bc44ed4133906ccf1a2efb33faafddc79769e858
JuanEstebanLG <56986294+JuanEstebanLG@users.noreply.github.com>
2023-07-04 22:04:45 -0500

    Add convertor API, create class to make request

---
+++ UniversalConvertor/src/main/java/UniversalConvertor.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+public class UniversalConvertor{
+    public static void main(String[] args) {
+        String uno[] = {"USD", "COP", ""};
+        String dos[] = {"UNO", "DOS"};
+        new MakeWindow(uno, dos);
+    }
+}
+
+
