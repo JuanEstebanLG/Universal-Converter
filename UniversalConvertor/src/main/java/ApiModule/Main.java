768bb238fe95f62cc12344a743f8a41a464b5f6d
JuanEstebanLG <56986294+JuanEstebanLG@users.noreply.github.com>
2023-07-18 18:05:28 -0500

    BETA RELEASE

--- UniversalConvertor/src/main/java/ApiModule/Main.java
+++ UniversalConvertor/src/main/java/ApiModule/Main.java
@@ -2,8 +2,17 @@ package ApiModule;
 
 import FormsModule.CoinsGUI;
 
-public class ApiRequest  {
+/**
+ * UniversalConvertor - Transformador por unidades.
+ * Proyecto ideado por @AluraLatam, Complementado y Elaborado por @JuanEstebanLG.
+ *
+ * @author Esteban
+ * @version 1.0
+ *
+ */
+public class Main {
     public static void main(String[] args) {
+
         String[] DIVISAS = {
                 "COP", "USD", "EUR", "JPY", "GBP", "AUD", "CAD", "CHF", "CNY", "SEK",
                 "NZD", "MXN", "SGD", "HKD", "NOK", "KRW", "TRY", "RUB", "INR", "BRL", "GBP"
